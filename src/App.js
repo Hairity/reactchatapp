0a70a1ff3c9cb49b97163169ff600104f7a9b160
Rauno Uustalu <raunoize@gmail.com>
2021-01-31 16:02:41 +0200

    components, chatengine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { ChatEngine } from 'react-chat-engine'
 
+import ChatFeed from './components/ChatFeed'
+
 import './App.css'
 
 const App = () => {
   return (
     <ChatEngine
       height='100vh'
-      projectID=''
-      userName=''
-      userSecret=''
-
+      projectID="
+8216286b-3d31-438f-9a42-bcbf7cef63bc"
+      userName="Willy Wonka"
+      userSecret="power"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 
 
     />
