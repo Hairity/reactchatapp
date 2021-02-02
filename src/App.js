680db6193e5c58af58fefa0fc959cfd5ee38fe45
Rauno Uustalu <raunoize@gmail.com>
2021-02-02 15:36:06 +0200

    final push, react app done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { ChatEngine } from 'react-chat-engine'
 
 import ChatFeed from './components/ChatFeed'
-
+import LoginForm from './components/LoginForm'
 import './App.css'
 
+const projectID="8216286b-3d31-438f-9a42-bcbf7cef63bc"
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
-      height='100vh'
-      projectID="
-8216286b-3d31-438f-9a42-bcbf7cef63bc"
-      userName="Willy Wonka"
-      userSecret="power"
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-
-
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
-  )
-}
+  );
+};
 
 export default App
